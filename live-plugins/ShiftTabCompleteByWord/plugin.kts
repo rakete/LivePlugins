5259e074ca0b93b92e480ddc8d1ff88222ecabc8
Andreas Raster <lazor@affenbande.org>
2025-01-11 04:32:37 +0100

    implement ShiftTabCompleteByWord

---
+++ live-plugins/ShiftTabCompleteByWord/plugin.kts
@@ -0,0 +1,49 @@
+﻿import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.DumbAware
+import liveplugin.registerAction
+import liveplugin.show
+
+class ShiftTabCompleteByWord: AnAction(), DumbAware {
+    val actionManager = ActionManager.getInstance()
+
+    fun performAction(event: AnActionEvent, name: String) {
+        val dataContext = event.dataContext
+        val inputEvent = event.inputEvent
+
+        val action = actionManager.getAction(name)
+        if (action != null) {
+            val actionEvent = AnActionEvent.createEvent(action, dataContext, null, event.place, ActionUiKind.NONE, inputEvent)
+            action.actionPerformed(actionEvent)
+        }
+    }
+
+    fun completeByWord(event: AnActionEvent) {
+        performAction(event, "InsertInlineCompletionWordAction")
+    }
+
+    fun suggestAiCompletion(event: AnActionEvent) {
+        performAction(event, "CallInlineCompletionAction")
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val editor = event.getData(CommonDataKeys.EDITOR)
+        if (editor != null) {
+            val caret = editor.caretModel!!.primaryCaret
+
+            val positionStart = caret!!.visualPosition
+            val startColumn = positionStart!!.column
+
+            completeByWord(event)
+
+            val positionAfterCompletionAction = caret!!.visualPosition
+            val afterCompletionColumn = positionAfterCompletionAction!!.column
+
+            if (afterCompletionColumn == startColumn) {
+                suggestAiCompletion(event)
+            }
+        }
+    }
+}
+
+registerAction(id = "ShiftTabCompleteByWord", keyStroke = "shift TAB", action = ShiftTabCompleteByWord())
+if (!isIdeStartup) show("Enabled ShiftTabCompleteByWord")
