d1dfa277bec2ec7505e3d9cb933b092bb8921144
Andreas Raster <lazor@affenbande.org>
2025-01-19 22:31:16 +0100

    add YankIndent

---
+++ live-plugins/YankIndent/plugin.kts
@@ -0,0 +1,42 @@
+﻿import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.openapi.project.DumbAware
+import liveplugin.registerAction
+import liveplugin.show
+import java.awt.datatransfer.StringSelection
+
+
+class YankIndentAction : AnAction(), DumbAware {
+    private val actionManager = ActionManager.getInstance()
+
+    fun performAction(event: AnActionEvent, actionId: String) {
+        val dataContext = event.dataContext
+        val inputEvent = event.inputEvent
+
+        val action = actionManager.getAction(actionId)
+        if (action != null) {
+            val actionEvent = AnActionEvent.createEvent(
+                action,
+                dataContext,
+                null,
+                event.place,
+                ActionUiKind.NONE,
+                inputEvent
+            )
+            action.actionPerformed(actionEvent)
+        }
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        performAction(event, "EditorUp")
+        performAction(event, "EditorLineEnd")
+        performAction(event, "EditorEnter")
+        performAction(event, "\$Paste")
+        performAction(event, "EmacsStyleIndent")
+
+    }
+}
+
+// Register the YankIndent action and bind it to a specific shortcut
+registerAction(id = "YankIndent", keyStroke = "ctrl Y", action = YankIndentAction())
+if (!isIdeStartup) show("Enabled YankIndent")
