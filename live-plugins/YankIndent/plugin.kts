ff799c9b007a5bcda987791cabdd078dbb72755f
Andreas Raster <lazor@affenbande.org>
2025-01-19 23:42:03 +0100

    make it so I can paste multiple lines with YankIndent

--- live-plugins/YankIndent/plugin.kts
+++ live-plugins/YankIndent/plugin.kts
@@ -3,7 +3,7 @@ import com.intellij.openapi.ide.CopyPasteManager
 import com.intellij.openapi.project.DumbAware
 import liveplugin.registerAction
 import liveplugin.show
-import java.awt.datatransfer.StringSelection
+import java.awt.datatransfer.DataFlavor
 
 
 class YankIndentAction : AnAction(), DumbAware {
@@ -27,13 +27,41 @@ class YankIndentAction : AnAction(), DumbAware {
         }
     }
 
-    override fun actionPerformed(event: AnActionEvent) {
-        performAction(event, "EditorUp")
-        performAction(event, "EditorLineEnd")
-        performAction(event, "EditorEnter")
-        performAction(event, "\$Paste")
-        performAction(event, "EmacsStyleIndent")
+    /*fun isCurrentLineEmpty(editor: Editor): Boolean {
+        val caretModel = editor.caretModel
+        val currentLine = caretModel.logicalPosition.line
 
+        val document = editor.document
+        if (currentLine < 0 || currentLine >= document.lineCount) {
+            return false
+        }
+
+        val lineStartOffset = document.getLineStartOffset(currentLine)
+        val lineEndOffset = document.getLineEndOffset(currentLine)
+        val lineText = document.charsSequence.substring(lineStartOffset, lineEndOffset)
+
+        return lineText.trim().isEmpty()
+    }*/
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val editor = event.getData(CommonDataKeys.EDITOR) ?: return
+
+        val clipboardContent = CopyPasteManager.getInstance().getContents<String>(DataFlavor.stringFlavor)
+        val n = clipboardContent?.count { it == '\n' } ?: 0
+        if (n == 0) {
+            performAction(event, "EditorUp")
+            performAction(event, "EditorLineEnd")
+            performAction(event, "EditorEnter")
+        }
+
+        val a = editor.caretModel.offset
+        performAction(event, "\$Paste")
+        val b = editor.caretModel.offset
+
+        val selectionModel = editor.selectionModel
+        selectionModel.setSelection(a, b)
+
+        performAction(event, "EmacsStyleIndent")
     }
 }
 
