17cc8b82db57cc73223bd6c2b033513d9cea40d3
Andreas Raster <lazor@affenbande.org>
2025-02-05 17:02:53 +0100

    fix tab in python

--- live-plugins/TabIndentOrComplete/plugin.kts
+++ live-plugins/TabIndentOrComplete/plugin.kts
@@ -7,6 +7,8 @@ import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileTypes.FileTypeManager
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.vfs.VirtualFile
+/*import com.intellij.openapi.wm.IdeFocusManager
+import com.intellij.terminal.JBTerminalPanel*/
 import liveplugin.registerAction
 import liveplugin.show
 import java.util.*
@@ -71,11 +73,54 @@ class TabIndentOrCompleteAction: AnAction(), DumbAware {
         return lineText.trim().isEmpty()
     }
 
+    fun isEndOfWord(editor: Editor): Boolean {
+        val caretModel = editor.caretModel
+        val offset = caretModel.offset
+
+        if (offset == 0) return false // No character behind caret
+
+        val document = editor.document
+        val charBeforeCaret = document.charsSequence[offset - 1]
+        val charAfterCaret = document.charsSequence[offset]
+        return (Character.isLetterOrDigit(charBeforeCaret) || charBeforeCaret == '_' || charBeforeCaret == '-') && !Character.isLetterOrDigit(
+            charAfterCaret
+        )
+    }
+
+    fun isBeforeCaretEmpty(editor: Editor): Boolean {
+        val caretModel = editor.caretModel
+        val currentLine = caretModel.logicalPosition.line
+
+        val document = editor.document
+        if (currentLine < 0 || currentLine >= document.lineCount) {
+            return false
+        }
+
+        val lineStartOffset = document.getLineStartOffset(currentLine)
+        val offset = caretModel.offset
+        val lineText = document.charsSequence.substring(lineStartOffset, offset)
+
+        return lineText.trim().isEmpty()
+    }
+
     override fun actionPerformed(event: AnActionEvent) {
+        //show("TAB")
+        /*val project = event.project
+        val focusManager = IdeFocusManager.getInstance(project)
+        val focusedComponent = focusManager.focusOwner
+        if (focusedComponent is JBTerminalPanel) {
+            show(focusedComponent)
+            performAction(event, "Terminal.CommandCompletion")
+            return
+        }*/
+
         val editor = event.getData(CommonDataKeys.EDITOR)
         val caret = editor!!.caretModel.primaryCaret
         val positionStart = caret.visualPosition
         val startColumn = positionStart.column
+
+        val document = editor.document
+        val virtualFile = FileDocumentManager.getInstance().getFile(document)
 
         if (caret.hasSelection()) {
             // - if there is an active selection, auto indent the region and
@@ -96,8 +141,6 @@ class TabIndentOrCompleteAction: AnAction(), DumbAware {
             val completionIndicator = completionService.currentCompletion as? CompletionProgressIndicator
             if (completionIndicator == null) {
                 insertTab(event)
-                val document = editor.document
-                val virtualFile = FileDocumentManager.getInstance().getFile(document)
                 if (!isPythonFile(virtualFile)) {
                     emacsIndent(event)
                 }
@@ -120,22 +163,29 @@ class TabIndentOrCompleteAction: AnAction(), DumbAware {
             if (afterLineStartColumn == 0) {
                 gotoLineStart(event)
             }
-            // - emacsIndent should indent according to the current language, or if the
-            // indentation is already correct or the language is not supported then it
-            // does nothing
-            val document = editor.document
-            val virtualFile = FileDocumentManager.getInstance().getFile(document)
-            if (!isPythonFile(virtualFile)) {
-                emacsIndent(event)
-            }
 
-            val afterIndentColumn = editor.caretModel.primaryCaret.visualPosition.column
-            // - if we're still at the same position after emacsIndent, then it did nothing
-            // and we can try to complete
-            if (afterIndentColumn == afterLineStartColumn && startColumn != 0) {
-                // - to complete we move caret back where we started and then complete
+            if (!isPythonFile(virtualFile)) {
+                // - emacsIndent should indent according to the current language, or if the
+                // indentation is already correct or the language is not supported then it
+                // does nothing
+
+                emacsIndent(event)
+
+                val afterIndentColumn = editor.caretModel.primaryCaret.visualPosition.column
+                // - if we're still at the same position after emacsIndent, then it did nothing
+                // and we can try to complete
                 editor.caretModel.moveToVisualPosition(positionStart)
-                complete(event)
+                if (afterIndentColumn == afterLineStartColumn && startColumn != 0) {
+                    // - to complete we move caret back where we started and then complete
+                    complete(event)
+                }
+            } else {
+                editor.caretModel.moveToVisualPosition(positionStart)
+                if (isEndOfWord(editor)) {
+                    complete(event)
+                } else if (isBeforeCaretEmpty(editor)) {
+                    insertTab(event)
+                }
             }
         }
     }
